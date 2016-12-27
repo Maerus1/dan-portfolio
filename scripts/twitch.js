5d039fa54885f382ffb587cef1780cec1f327352
Daniel Dorsey <danieldorsey472@gmail.com>
2016-12-26 21:04:54 -0500

    changed icon organization
    made the images img-responsive and took away link text. Link is on pic
    right now and put three images in each row. Begun to start on the text
    input fields

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -1,18 +1,34 @@
 $(function(){
   var content = $(".content");
+  var counter = 0;
   //create list of users array for future population of the webpage
   var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+  //inital setup of the main container
+  content.append(
+    "<div class=\"row text-left \">"
+  );
+
   for(var i in users){
     (function(i){
       $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + users[i], function(obj){
         content.append(
-          "<div class=\"row text-left\">" +
-          "<img src=\"" + obj.logo + "\" alt=\"User's logo\" width=\"75\" height=\"75\" style=\"border-radius:50%\">" +
-          "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" + obj.display_name + "</a>" +
-          "</div><br>"
+          "<div class=\"col-lg-4 col-md-4 col-sm-4 col-xs-4\">" +
+          "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" +
+          "<img class=\"img-responsive img-circle\" src=\"" + obj.logo + "\" alt=\"User's logo\">" +
+          "</a>" +
+          "</div>"
         );
+        if(counter == 3){
+          content.append(
+            "</div><br>" +
+            "<div class=\"row\">"
+          );
+          counter = 0;
+        }
         console.log(obj);
       });
+      counter++;
     })(i);
   }
 });
