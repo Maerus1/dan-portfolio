7fabb73d197a3c7429229444d1fadd10eb561694
Daniel Dorsey <danieldorsey472@gmail.com>
2016-12-26 22:06:24 -0500

    Added search and addUser functionality
    Users can now look up streamers and add new streamers to their list.
    Next up is using cookies to remember these changes?

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -1,34 +1,75 @@
 $(function(){
-  var content = $(".content");
+  var userList = $(".userList");
   var counter = 0;
+  var searchUser = $("#searchUser");
+  var addUser = $("#addUser");
+  var removeUser = $("#removeUser");
+
   //create list of users array for future population of the webpage
-  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var users = ["esl_sc2", "ogamingsc2", "cretetion", "freecodecamp", "storbeck", "habathcx", "robotcaleb", "noobs2ninjas"];
 
-  //inital setup of the main container
-  content.append(
-    "<div class=\"row text-left \">"
-  );
+  function recall(){
+    //inital setup of the main container
+    userList.html(
+      "<div class=\"row text-center\">"
+    );
+    for(var i in users){
+      (function(i){
+        $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + users[i], function(obj){
 
-  for(var i in users){
-    (function(i){
-      $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + users[i], function(obj){
-        content.append(
-          "<div class=\"col-lg-4 col-md-4 col-sm-4 col-xs-4\">" +
-          "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" +
-          "<img class=\"img-responsive img-circle\" src=\"" + obj.logo + "\" alt=\"User's logo\">" +
-          "</a>" +
-          "</div>"
-        );
-        if(counter == 3){
-          content.append(
-            "</div><br>" +
-            "<div class=\"row\">"
-          );
-          counter = 0;
-        }
-        console.log(obj);
-      });
-      counter++;
-    })(i);
+          if(obj.logo !== null){
+            userList.append(
+              "<div class=\"col-lg-4 col-md-4 col-sm-4 col-xs-4\">" +
+              "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" +
+              "<img class=\"img-responsive img-circle center-block\" src=\"" + obj.logo + "\" alt=\"User's logo\">" +
+              "<p>" + obj.name + "</p></a>" +
+              "</div>"
+            );
+          }
+          else{
+             userList.append(
+               "<div class=\"col-lg-4 col-md-4 col-sm-4 col-xs-4\">" +
+               "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" +
+               "<img class=\"img-responsive img-circle center-block\" src=\"../images/broken_image.png\" alt=\"User's logo\">" +
+               "<p>" + obj.name + "</p></a>" +
+               "</div>"
+             );
+          }
+
+          if(counter == 3){
+            userList.append(
+              "</div><br>" +
+              "<div class=\"row text-center\">"
+            );
+            counter = 0;
+          }
+          console.log(obj);
+        });
+        counter++;
+      })(i);
+    }
   }
+
+  //add searching functionality
+  $(searchUser).keyup(function(){
+    if($(this).val() !== ""){
+      $("p:not(:contains(" + $(this).val() + "))").closest("div").hide();
+    }
+    else {
+      $("p").closest("div").show()
+    }
+  });
+
+  //add a user
+  $(addUser).keypress(function(){
+    if(event.which === 13){
+      users.push(addUser.val().toLowerCase());
+      console.log(users);
+      addUser.val("");
+      //make a call to the twitch api
+      recall();
+    }
+  });
+  //initial call to the twitch API
+  recall();
 });
