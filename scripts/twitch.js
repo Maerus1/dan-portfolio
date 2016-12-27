26713eeb06ebff6f4bc81bd7312f824cb4a24e27
Daniel Dorsey <danieldorsey472@gmail.com>
2016-12-26 19:59:35 -0500

    Added style containers
    I added some rows to my streamer list and contained it within a centered
    container using bootstrap.

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -6,8 +6,10 @@ $(function(){
     (function(i){
       $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + users[i], function(obj){
         content.append(
+          "<div class=\"row text-left\">" +
           "<img src=\"" + obj.logo + "\" alt=\"User's logo\" width=\"75\" height=\"75\" style=\"border-radius:50%\">" +
-          "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" + obj.display_name + "</a>"
+          "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" + obj.display_name + "</a>" +
+          "</div><br>"
         );
         console.log(obj);
       });
