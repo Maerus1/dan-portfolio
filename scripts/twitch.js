7e22e67c2852c866bf69a4a494876670c2171e9e
Daniel Dorsey <danieldorsey472@gmail.com>
2016-12-26 23:04:30 -0500

    completed field functionality
    You can now delete streamers from your list, add streamers and search
    for streamers. All fields are case insensitive, but you must spell
    things right.

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -13,10 +13,15 @@ $(function(){
     userList.html(
       "<div class=\"row text-center\">"
     );
+    counter = 0;
     for(var i in users){
       (function(i){
         $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + users[i], function(obj){
-
+          if(obj.name == undefined){
+            return;
+          }
+          console.log(counter);
+          counter++;
           if(obj.logo !== null){
             userList.append(
               "<div class=\"col-lg-4 col-md-4 col-sm-4 col-xs-4\">" +
@@ -30,7 +35,7 @@ $(function(){
              userList.append(
                "<div class=\"col-lg-4 col-md-4 col-sm-4 col-xs-4\">" +
                "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" +
-               "<img class=\"img-responsive img-circle center-block\" src=\"../images/broken_image.png\" alt=\"User's logo\">" +
+               "<img class=\"img-responsive img-circle center-block\" src=\"../images/broken_image.png\" width=\"300px\" alt=\"User's logo\">" +
                "<p>" + obj.name + "</p></a>" +
                "</div>"
              );
@@ -45,7 +50,6 @@ $(function(){
           }
           console.log(obj);
         });
-        counter++;
       })(i);
     }
   }
@@ -53,7 +57,7 @@ $(function(){
   //add searching functionality
   $(searchUser).keyup(function(){
     if($(this).val() !== ""){
-      $("p:not(:contains(" + $(this).val() + "))").closest("div").hide();
+      $("p:not(:contains(" + $(this).val().toLowerCase() + "))").closest("div").hide();
     }
     else {
       $("p").closest("div").show()
@@ -66,6 +70,21 @@ $(function(){
       users.push(addUser.val().toLowerCase());
       console.log(users);
       addUser.val("");
+      //make a call to the twitch api
+      recall();
+    }
+  });
+
+  //remove a user
+  $(removeUser).keypress(function(){
+    if(event.which === 13){
+      for(var i = 0; i < users.length; i++){
+        if(users[i] == (removeUser.val()).toLowerCase()){
+          users.splice(i, 1);
+        }
+      }
+      console.log(users);
+      removeUser.val("");
       //make a call to the twitch api
       recall();
     }
