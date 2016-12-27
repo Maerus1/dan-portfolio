78b3696cb9fccdb1da4563c9c0e0959aa462c642
Daniel Dorsey <danieldorsey472@gmail.com>
2016-12-26 19:45:44 -0500

    Added name, link and image to page
    Added funcitonality where user can see a list of streamers and be able
    to click on their name to be redirected to their twitch channel.

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -1,10 +1,14 @@
 $(function(){
-
+  var content = $(".content");
   //create list of users array for future population of the webpage
   var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   for(var i in users){
     (function(i){
       $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + users[i], function(obj){
+        content.append(
+          "<img src=\"" + obj.logo + "\" alt=\"User's logo\" width=\"75\" height=\"75\" style=\"border-radius:50%\">" +
+          "<a href=\"https://www.twitch.tv/" + obj.name + "\" target=\"_blank\">" + obj.display_name + "</a>"
+        );
         console.log(obj);
       });
     })(i);
