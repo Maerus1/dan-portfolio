75d2ba14bf8881431d95d1f8f5a3f8236b719b40
Daniel Dorsey <danieldorsey472@gmail.com>
2017-01-04 19:05:53 -0500

    renamed files and updated info
    "wanikani" files are now names "todo", I have updated all the navs and
    titles, as well as the content of the 'todo' files to reflect this
    change.

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,7 +8,8 @@ if(localStorage.getItem("tasks") !== null){
 else{
   items = {
     "titles": [],
-    "times": []
+    "times": [],
+    "side": []
   };
   console.log(items);
 }
@@ -68,7 +69,6 @@ $(function(){
       "</div>"
     );
   }
- //FIX ME!
 
   //add a button press event here!
   addItem.click(function(){
@@ -90,38 +90,38 @@ $(function(){
   });
 
   //Wanikani getJSON
+  function useWaniKani(){    
+    $.getJSON("https://www.wanikani.com/api/user/70ae2325dad1f0eb84324d57ace46c7a/study-queue?callback=?", function(obj){
+      console.log(obj);
 
+      //if the user exists, then retreive the info
+      if(obj.user_information !== undefined){
+        $.getJSON("https://www.gravatar.com/" + obj.user_information.gravatar + ".json?callback=?", function(obj){
+          console.log(obj);
+          image.attr("src", obj.entry[0].photos[0].value);
+        });
+        name.html(obj.user_information.username);
+        level.append(obj.user_information.level);
 
-  $.getJSON("https://www.wanikani.com/api/user/70ae2325dad1f0eb84324d57ace46c7a/study-queue?callback=?", function(obj){
-    console.log(obj);
+        //find the next review date
+        var d = new Date(0);
+        d.setUTCSeconds(obj.requested_information.next_review_date);
+        //add the wanikani default task
+        //ALSO ADD IF STATEMENT HERE TO MAKE SURE ONLY DAILY TASKS ARE ADDED
+        tasks.append(
+          "<div draggable=\"true\" id=\"drag" + counter++ + "\" ondragstart=\"drag(event)\" style=\"background-color: #16213E;border-radius: 10px;border: 1px solid #444D65;\">" +
+          "<p>Wanikani Review</p>" +
+          "<p>Target Time: " + d.toLocaleTimeString() + "</p>" +
+          "</div>"
+        );
+      }
+      //if not, use placeholder values
+      else{
+        name.html(obj.error.message);
+      }
 
-    //if the user exists, then retreive the info
-    if(obj.user_information !== undefined){
-      $.getJSON("https://www.gravatar.com/" + obj.user_information.gravatar + ".json?callback=?", function(obj){
-        console.log(obj);
-        image.attr("src", obj.entry[0].photos[0].value);
-      });
-      name.html(obj.user_information.username);
-      level.append(obj.user_information.level);
-
-      //find the next review date
-      var d = new Date(0);
-      d.setUTCSeconds(obj.requested_information.next_review_date);
-      //add the wanikani default task
-      //ALSO ADD IF STATEMENT HERE TO MAKE SURE ONLY DAILY TASKS ARE ADDED
-      tasks.append(
-        "<div draggable=\"true\" id=\"drag" + counter++ + "\" ondragstart=\"drag(event)\" style=\"background-color: #16213E;border-radius: 10px;border: 1px solid #444D65;\">" +
-        "<p>Wanikani Review</p>" +
-        "<p>Target Time: " + d.toLocaleTimeString() + "</p>" +
-        "</div>"
-      );
-    }
-    //if not, use placeholder values
-    else{
-      name.html(obj.error.message);
-    }
-
-  }).fail(function(obj){
-    console.log("You have encountered an invalid host");
-  });
+    }).fail(function(obj){
+      console.log("You have encountered an invalid host");
+    });
+  }
 });
