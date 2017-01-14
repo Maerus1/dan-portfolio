f94f903a1ab03357a6428b24d73d35673b6a23ae
Daniel Dorsey <danieldorsey472@gmail.com>
2017-01-14 14:06:46 -0500

    Reverted todo list to remove Wanikani and restore localstorage usage
    I broke the array I was using for local storage, so I fixed that issue
    and I removed the Wanikani functionality.

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,13 +8,9 @@ if(localStorage.getItem("tasks") !== null){
 else{
   items = {
     "titles": [],
-    "times": [],
-    "side": []
+    "times": []
   };
-  console.log(items);
 }
-
-
 //functionality for dragging and dropping elements
 function drag(ev){
   ev.dataTransfer.setData("text", ev.target.id);
@@ -88,40 +84,4 @@ $(function(){
       localStorage.setItem("tasks", JSON.stringify(items));
     }
   });
-
-  //Wanikani getJSON
-  function useWaniKani(){    
-    $.getJSON("https://www.wanikani.com/api/user/70ae2325dad1f0eb84324d57ace46c7a/study-queue?callback=?", function(obj){
-      console.log(obj);
-
-      //if the user exists, then retreive the info
-      if(obj.user_information !== undefined){
-        $.getJSON("https://www.gravatar.com/" + obj.user_information.gravatar + ".json?callback=?", function(obj){
-          console.log(obj);
-          image.attr("src", obj.entry[0].photos[0].value);
-        });
-        name.html(obj.user_information.username);
-        level.append(obj.user_information.level);
-
-        //find the next review date
-        var d = new Date(0);
-        d.setUTCSeconds(obj.requested_information.next_review_date);
-        //add the wanikani default task
-        //ALSO ADD IF STATEMENT HERE TO MAKE SURE ONLY DAILY TASKS ARE ADDED
-        tasks.append(
-          "<div draggable=\"true\" id=\"drag" + counter++ + "\" ondragstart=\"drag(event)\" style=\"background-color: #16213E;border-radius: 10px;border: 1px solid #444D65;\">" +
-          "<p>Wanikani Review</p>" +
-          "<p>Target Time: " + d.toLocaleTimeString() + "</p>" +
-          "</div>"
-        );
-      }
-      //if not, use placeholder values
-      else{
-        name.html(obj.error.message);
-      }
-
-    }).fail(function(obj){
-      console.log("You have encountered an invalid host");
-    });
-  }
 });
